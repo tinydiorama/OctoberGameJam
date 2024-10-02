3b25db501127c1d8c4e262141f226b1a53de174f
Neyl <assassinloup@gmail.com>
2024-10-03 00:53:46 +0200

    Update grid.cs
    Updated grid with getter/setter and debug functions

--- JAMVending/Assets/Scripts/Grid.cs
+++ JAMVending/Assets/Scripts/Grid.cs
@@ -6,14 +6,19 @@ public class Grid
 {
     private int m_width;
     private int m_height;
+    private Vector3 m_originPosition;
     private float m_cellSize;
     private int[,] m_gridArray;
-    public Grid(int width, int height, float cellSize)
+    private TextMesh[,] debugTextArray;
+
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
     {
         m_width = width;
         m_height = height;
         m_cellSize = cellSize;
         m_gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+        m_originPosition = originPosition;
         Debug.Log(width + " " + height);
 
 
@@ -21,14 +26,60 @@ public class Grid
         {
             for (int y = 0; y < m_gridArray.GetLength(1); y++)
             {
-                CreateWorldText(m_gridArray[x,y].ToString(), Color.black, null,  GetWorldPosition(x,y), 20);
+                debugTextArray[x, y] = CreateWorldText(m_gridArray[x, y].ToString(), Color.black, null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 20);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
             }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+        SetValue(1, 1, 4);
+
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * m_cellSize + m_originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - m_originPosition).x / m_cellSize);
+        y = Mathf.FloorToInt((worldPosition -m_originPosition).y / m_cellSize);
+
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < m_width && y < m_height)
+        {
+            m_gridArray[x, y] = value;
+            debugTextArray[x, y].text = m_gridArray[x, y].ToString();
+
         }
     }
 
-    private Vector3 GetWorldPosition( int x, int y )
+    public void SetValue(Vector3 worldPosition, int value)
     {
-        return new Vector3(x, y) * m_cellSize;
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < m_width && y < m_height)
+            return m_gridArray[x, y];
+        else
+            return -1;
+
+    }
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
     }
     #region DebugUtils
     public static TextMesh CreateWorldText(string text, Color color, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, TextAnchor textAnchor = TextAnchor.MiddleCenter, TextAlignment textAlignment = TextAlignment.Center, int sortingOrder = 1)
--- JAMVending/Assets/StupidThingsForTest/TestingGrind.cs
+++ JAMVending/Assets/StupidThingsForTest/TestingGrind.cs
@@ -1,12 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TestingGrind : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private Grid grid;
     private void Start()
     {
-        Grid grid = new Grid(4, 2, 10f);
+        grid = new Grid(40, 20, 10f, new Vector3(0,0,0));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(GetMouseWorldPosition(), 30);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(GetMouseWorldPosition()));
+        }
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0;
+        return vec;
+            
+    }
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
     }
 }
